f95938ebd0816a6ee6cdd8571326a644a7c252d3
sahsanaly <s3736294@student.rmit.edu.au>
2022-09-09 21:31:23 +1000

    Initial commit

---
+++ src/main/java/com/sept/challenge1/Challenge1Application.java
@@ -0,0 +1,13 @@
+package com.sept.challenge1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Challenge1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Challenge1Application.class, args);
+    }
+
+}
