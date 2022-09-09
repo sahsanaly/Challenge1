79f527ca4cffcc6f1ea1bc5d19bfaea202e9e29a
sahsanaly <s3736294@student.rmit.edu.au>
2022-09-09 21:52:29 +1000

    Model done

---
+++ src/main/java/com/sept/challenge1/model/Items.java
@@ -0,0 +1,20 @@
+package com.sept.challenge1.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Items
+{
+    private List<Item> itemList;
+
+    public List<Item> getItemList() {
+        if(itemList == null) {
+            itemList = new ArrayList<>();
+        }
+        return itemList;
+    }
+
+    public void setItemList(List<Item> itemList) {
+        this.itemList = itemList;
+    }
+}
